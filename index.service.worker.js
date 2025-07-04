4dd36decbc9f36783e132323d76b88c7eca346d1
hamid0740 <hamidd0740@gmail.com>
2025-07-04 04:25:02 +0330

    v1.2

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1751588863|353512395787';
+const CACHE_VERSION = '1751590104|354753801417';
 /** @type {string} */
 const CACHE_PREFIX = 'Thought of Fligh-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
